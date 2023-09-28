c999dbedcb07ba2d7f634c781468a6aebb70750e
fadelanumah <fadelanumah@gmail.com>
2023-09-29 05:05:46 +0700

    feat: Add Clear Button

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let mainContainer = document.querySelector(".container");
 
+// create div for box
+let boxGroup = document.createElement("div");
+boxGroup.classList.add('box-group');
+
 // create singleDiv as a box
 function createDiv(size) {
     let newDiv = document.createElement("div");
@@ -14,13 +18,40 @@ function createDiv(size) {
 }
 
 // create multiple box for 16x16
-
 function createMultipleDiv() {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
-            mainContainer.appendChild(createDiv(30));
+            boxGroup.appendChild(createDiv(30));
         }
     }
+    mainContainer.appendChild(boxGroup);
 }
 
-createMultipleDiv()
+// add background color when hovering the box
+function addBackgroundWhenHover() {
+    let boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = "black";
+        })
+    });
+}
+
+// clear canvas
+function clearcanvas() {
+    let clearButton = document.querySelector('#clearButton');
+
+    clearButton.addEventListener('click', () => {
+        let boxes = document.querySelectorAll('.box');
+
+        boxes.forEach(element => {
+            element.style.backgroundColor = "white";
+        });
+    })
+}
+
+// call all function needed
+createMultipleDiv();
+addBackgroundWhenHover();
+clearcanvas();
